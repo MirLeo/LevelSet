3ab37106a64cb80efdb1f0391c4519b8254a76fb
mirazimisaeed <mirazimisaeed@gmail.com>
2017-03-14 21:12:04 -0700

    Rotating circle advection

--- Driver.m
+++ Driver.m
@@ -13,25 +13,34 @@ x = -lx/2:dx:lx/2-dx;
 y = -ly/2:dy:ly/2-dy;
 [X,Y] = meshgrid(x,y);
 
-phi = sqrt(X.^2+Y.^2)-1;
+phi = sqrt((X-1).^2+(Y-1).^2)-0.2;
 
 contour(X,Y,phi,'showtext','on')
 axis equal
 
-u = X;
-v = Y;
+% u = X;
+% v = Y;
+u = -Y;
+v = X;
+Tfinal = 2*pi;
 quiver(X,Y,u,v)
 %G = numgrid('S',nx)
 %newphi
 newPhi = LevelSetEvolve(phi,v,u,nx,ny,dx,dy,dt);
-
-for k=1:1000
+contour(X,Y,phi,[0,0],'linewidth',2)
+xlabel('X')
+ylabel('Y')
+axis equal
+N = ceil(Tfinal/dt);
+for k=1:N
     time = k*dt;
     newPhi = LevelSetEvolve(phi,v,u,nx,ny,dx,dy,dt);
     phi = newPhi;
-    contourf(X,Y,phi)
+%     contourf(X,Y,phi)
     colorbar
-%     contour(X,Y,phi,[0,0])
+    contour(X,Y,phi,[0,0])
+    xlabel('X')
+    ylabel('Y')
 %     contour(X,Y,phi,'showtext','on')
     axis equal
     title(sprintf('time=%d',time))
